08a97768ab97fe5b694c1f6ef998ec86767ac35a
Mohit Srivastava <iammohitpro@gmail.com>
2022-04-11 00:32:06 +0530

    safecommit

---
+++ server/controllers/auth.js
@@ -0,0 +1,81 @@
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../utils/auth";
+import jwt from "jsonwebtoken";
+
+export const register = async (req, res) => {
+  try {
+    //console.log(req.body);
+    const { name, email, password } = req.body;
+    //validation
+    if (!name) return res.status(400).send("Name is required");
+    if (!password || password.length < 6) {
+      return res
+        .status(400)
+        .send("Password is required & should be min 6 characters long");
+    }
+
+    let userExist = await User.findOne({ email }).exec();
+    if (userExist) return res.status(400).send("Email is taken");
+
+    //hash password
+    const hashedPassword = await hashPassword(password);
+
+    //register
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+    //console.log("saved user", user);
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again!");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    //console.log(req.body);
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).exec();
+    if (!user) return res.status(400).send("No user found");
+    //check pass
+    const match = await comparePassword(password, user.password);
+    //create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    // return user and token to client
+    user.password = undefined;
+    // send token in cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+    });
+    // send user as json response
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again!");
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("token");
+    return res.json({ message: "Signout success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password").exec();
+    return res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+  }
+};
